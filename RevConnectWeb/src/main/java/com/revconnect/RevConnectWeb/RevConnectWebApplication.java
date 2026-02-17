9e9d619fc11dca195f9bce4e0c23a930f2f9987f
romanvaibhav <vaibhavroman249@gmail.com>
2026-02-17 23:25:40 +0530

    Backend Folder Structure

---
+++ RevConnectWeb/src/main/java/com/revconnect/RevConnectWeb/RevConnectWebApplication.java
@@ -0,0 +1,13 @@
+package com.revconnect.RevConnectWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevConnectWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevConnectWebApplication.class, args);
+	}
+
+}
